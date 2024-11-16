128b515dd9de12c4fbeb2d263c4adff88d4faa19
NavadeepDj <142133431+NavadeepDj@users.noreply.github.com>
2024-11-16 23:49:37 +0530

    Update server.py - Made resizing and New best 15pt

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # Load your YOLOv8 model
-model = YOLO('best (15).pt')  # Replace with your model path
+model = YOLO('best (9).pt')  # Replace with your model path
 
 @app.route('/detect_person', methods=['POST'])
 def detect_person():
@@ -23,6 +23,8 @@ def detect_person():
         # Convert base64 image to PIL Image
         image_bytes = base64.b64decode(image_data.split(',')[1])
         image = Image.open(io.BytesIO(image_bytes))
+        image = image.resize((640, 640))  # Resize to match the model’s expected input
+
         
         # Run inference on the image
         results = model(image)  # Run the YOLO model
